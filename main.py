5a4465282abd7ad20002cac7d95bcc414c5edf88
bstutenroth <demouser@MacBook-Air.local>
2017-07-24 10:57:13 -0700

    display form on local host

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ import webapp2
 import jinja2
 import os
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('templates/submit.html')
-        self.response.write(template.render())
-    def post (self):
-        results_template = env.get_template('results.html')
-        template_variables = {
+    def get (self):
+        template = env.get_template('newfood.html')
+        newfood_variables = {
             'name':self.request.get("name"),
             'category':self.request.get("category"),
             'expire_date':self.request.get("expire_date"),
         }
-        self.response.out.write(results_template.render(template_variables))
+        self.response.out.write(template.render(newfood_variables))
 
 
 app = webapp2.WSGIApplication([
