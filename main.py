20f2e34253266e651a67148e4016744738699ed0
allisonhtun <allisonhtun@gmail.com>
2017-07-24 11:43:05 -0700

    homepage

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import webapp2
 import jinja2
 import os
+
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
-class MainHandler(webapp2.RequestHandler):
+class HomeHandler(webapp2.RequestHandler):
     def get (self):
-        template = env.get_template('newfood.html')
+        template = env.get_template('homepage.html')
+        self.response.out.write(template.render())
+
+class NewFoodHandler(webapp2.RequestHandler):
+    def get (self):
+        newfood_template = env.get_template('newfood.html')
         newfood_variables = {
             'name':self.request.get("name"),
             'category':self.request.get("category"),
             'expire_date':self.request.get("expire_date"),
         }
-        self.response.out.write(template.render(newfood_variables))
-
+        self.response.out.write(newfood_template.render(newfood_variables))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', HomeHandler),
+    ('/newfood', NewFoodHandler)
 ], debug=True)
