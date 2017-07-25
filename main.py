88a8669d592d64131a9ba2bf083133ce368cdf85
allisonhtun <allisonhtun@gmail.com>
2017-07-25 16:43:25 -0700

    Temp Calendar

--- main.py
+++ main.py
@@ -14,15 +14,23 @@ class HomeHandler(webapp2.RequestHandler):
     def get (self):
         user = users.get_current_user()
         if user:
-            email = user.nickname()
-
-            # current_user = User(user_id=email)
             greeting = ('<div class = "logout">Welcome, %s! (<a href="%s">sign out</a>)</div>' %
                 (user.nickname(), users.create_logout_url('/')))
         else:
             greeting = ('<div class = "login" ><a href="%s">Sign in or register</a></div>' %
                 users.create_login_url('/'))
-
+        all_users_query = User.query()
+        all_users = all_users_query.fetch()
+        print user.nickname()
+        print all_users
+        for get_user in all_users:
+            print get_user.email
+        #     if user.nickname():
+        #         new_user = User(email=user.nickname())
+        #         new_user.put()
+        #         all_users_query = User.query()
+        #         all_users = all_users_query.fetch()
+        #     print all_users
         self.response.write('<html><body>%s</body></html>' % greeting)
         template = env.get_template('homepage.html')
         self.response.out.write(template.render())
@@ -44,37 +52,36 @@ class NewFoodHandler(webapp2.RequestHandler):
         }
         brenna=User(name='brenna')
         brenna_key=brenna.put()
-        food1 = Food(user_key=brenna_key, foodname= submitted_variables['foodname'], date=datetime.strptime(submitted_variables['date'], '%Y-%m-%d'))
+        food1 = Food(user_key=brenna_key, foodname= submitted_variables['foodname'], date=submitted_variables['date'])
         food1_key=food1.put()
-
 
 #Displaying on Calendar Handler
 class ListofExpirationHandler(webapp2.RequestHandler):
     def get (self):
         # find who's the current user
-        current_user = users.get_current_user()
-        current_user_email = current_user.nickname()
+        # current_user = users.get_current_user()
+        # current_user_id = current_user.id()
+        # current_user_email = current_user.nickname()
 
         # make a query for the user whose email is current_user_emaul
         # my_user_query =
         # # fetch
         # my_user =
 
-        #will use these two lines with datastore
-        # food_list = Food.query(Food.user_key == curr_user.key).fetch()
+        # #will use these two lines with datastore
+        # food_list = Food.query(Food.user_key == my_user.key).fetch()
         # variables = {'food_list': food_list}
 
         #temp variable list to display on calendar while waiting on datastore
-        # temp_food = [{'foodname':'Chicken', 'category':'Meat', 'expire_date':datetime.date(2017,8,1)},
-        # {'foodname':'Milk', 'category':'Dairy', 'expire_date':datetime.date(2017,7,31)},
-        # {'foodname':'Grapes', 'category':'Fruit', 'expire_date':datetime.date(2017,7,28)}]
-        # temp_user = 'Brenna'
-
-        # #will attempt to sort temp items in temp_food by expire date
-        # temp_food.sort(key=lambda item:item['expire_date'], reverse=False)
-        # temp_variables = {'temp_user':temp_user, 'temp_food':temp_food}
-        # list_template = env.get_template('calendar.html')
-        # self.response.write(list_template.render(temp_variables))
+        temp_food = [{'foodname':'Chicken', 'date':datetime(2017,8,1)},
+        {'foodname':'Milk', 'date':datetime(2017,7,28)},
+        {'foodname':'Grapes', 'date':datetime(2017,7,31)}]
+        temp_user = 'Brenna'
+        #will attempt to sort temp items in temp_food by expire date
+        temp_food.sort(key=lambda item:item['date'], reverse=False)
+        temp_variables = {'temp_user':temp_user, 'temp_food':temp_food}
+        list_template = env.get_template('calendar.html')
+        self.response.write(list_template.render(temp_variables))
 
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
