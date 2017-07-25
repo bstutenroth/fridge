cd0ece126a0cb20eb90d7a9484fd8bb26c9ce2ca
allisonhtun <allisonhtun@gmail.com>
2017-07-25 16:06:05 -0700

    Working user datastore

--- food.py
+++ food.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
-    name = ndb.StringProperty()
+    email = ndb.StringProperty()
 
 class Food(ndb.Model):
     foodname = ndb.StringProperty()
